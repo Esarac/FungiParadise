6b5281f72aa43e93d48349690e24591598de9c12
Esarac <48232743+Esarac@users.noreply.github.com>
2020-10-07 15:18:25 -0500

    The classes are now available in the proyect

---
+++ Model/Mushroom.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace FungiParadise.Model
+{
+    class Mushroom
+    {
+        //Constants
+        public enum Type {Edible, Poisonous}
+        public static readonly char[] CAP_SHAPE = {'b', 'c', 'x', 'f', 'k', 's'};
+        //public static readonly char[] CAP_SHAPE = { 'b', 'c', 'x', 'f', 'k', 's' };
+
+        //Attributes
+        //Class
+        private Type type;
+            //Vars
+        private char capShape;
+        private char capSurface;
+        private char capColor;
+
+        private char bruises;
+        private char odor;
+
+        private char gillAttachment;
+        private char gillSpacing;
+        private char gillSize;
+        private char gillColor;
+
+        private char stalkShape;
+        private char stalkRoot;
+        private char stalkSurfaceAboveRing;
+        private char stalkSurfaceBelowRing;
+        private char stalkColorAboveRing;
+        private char stalkColorBelowRing;
+
+        private char veilType;
+        private char veilColor;
+
+        private char ringNumber;
+        private char ringType;
+
+        private char sporePrintColor;
+        private char population;
+        private char habitad;
+
+        //Constructor
+
+    }
+}
