38bba7d68edfa987a8ea2aa54332fa88b15ddc47
Esarac <48232743+Esarac@users.noreply.github.com>
2020-10-18 23:24:33 -0500

    I create only the decision tree class and the abstract class decision :no_good:

---
+++ Src/Util/Decision.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FungiParadise.Src.Util
+{
+    public abstract class Decision<T>
+    {
+        public abstract void Evaluate(T obj);
+    }
+}
