53e9f5f5608989a55f687533a2684b1cfb1e31b6
VoodLyc <48293069+VoodLyc@users.noreply.github.com>
2020-11-02 04:22:02 -0500

    I added a icon to the message screen

--- FungiParadise/Src/Gui/MessageClassify.Designer.cs
+++ FungiParadise/Src/Gui/MessageClassify.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MessageClassify));
             this.messageLabel = new MetroFramework.Drawing.Html.HtmlLabel();
             this.SuspendLayout();
             // 
@@ -50,6 +51,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(510, 556);
             this.Controls.Add(this.messageLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MessageClassify";
