4471e50f98fbcaebbb6a8ed3987903af7c71380c
Mateo Valdes <mavaldot@gmail.com>
2020-12-05 17:59:22 -0500

    Finished experiment.py

--- Experiment/experiment.py
+++ Experiment/experiment.py
@@ -11,3 +11,13 @@ from statsmodels.formula.api import ols
 df = pd.read_csv('results.csv')
 
 df.columns = df.columns.str.replace(' ', '_')
+
+dt_value_counts = df['Decision_Tree'].value_counts()
+
+variables = 'Accuracy ~ C(Decision_Tree)'
+
+model = ols(variables, df).fit()
+
+table = sm.stats.anova_lm(model, typ=2)
+
+print(table.to_string())
