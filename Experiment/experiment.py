18c91c10f40483554faa7821b1ff24493713ea4e
Mateo Valdes <mavaldot@gmail.com>
2020-12-05 19:12:03 -0500

    experiment.py now performs Two-Way Anova

--- Experiment/experiment.py
+++ Experiment/experiment.py
@@ -3,7 +3,6 @@
 
 @author: mateo
 """
-
 import pandas as pd
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
@@ -13,11 +12,14 @@ df = pd.read_csv('results.csv')
 df.columns = df.columns.str.replace(' ', '_')
 
 dt_value_counts = df['Decision_Tree'].value_counts()
+tq_value_counts = df['Train_Quantity'].value_counts()
 
-variables = 'Accuracy ~ C(Decision_Tree)'
+variables = 'Accuracy ~ C(Decision_Tree) + C(Train_Quantity) + C(Decision_Tree):C(Train_Quantity)'
 
 model = ols(variables, df).fit()
 
 table = sm.stats.anova_lm(model, typ=2)
 
 print(table.to_string())
+print('')
+print(df.groupby(['Decision_Tree', 'Train_Quantity'], as_index=False).mean())
